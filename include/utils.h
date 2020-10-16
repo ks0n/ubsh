7c45022cad9a57d1161fade484fefd482ade6b6f
Martin Schmidt <martin.schmidt@epita.fr>
2020-10-16 18:05:35 +0200

    Propose an API for a charstream. (#13)
    * Propose an API for a charstream.
    
    Once we have a charstream API, we can start writing a lexer API.
    
    * Use FILE stream instread of file descriptor
    
    * Update include/unimplemented.h
    
    * Rename unimplemented.h -> utils.h
    
    Co-authored-by: CohenArthur <arthur.cohen@epita.fr>

---
+++ include/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <err.h>
+#include <stdlib.h>
+
+#define UNIMPLEMENTED()  \
+	errx(EXIT_FAILURE, "function %s in %s:%d not implemented", __FUNCTION__, __FILE__, __LINE__)
+
+#endif /* ! UTILS_H */
---
+++ src/charstream.c
@@ -0,0 +1,15 @@
+#include "charstream.h"
+
+#include "utils.h"
+
+int charstream_init(struct charstream *cs, FILE *file)
+{
+	UNIMPLEMENTED();
+}
+char charstream_read(struct charstream *cs) {
+	UNIMPLEMENTED();
+}
+
+char charstream_peek(struct charstream *cs) {
+	UNIMPLEMENTED();
+}
