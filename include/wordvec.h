4179304380143a1aaa89a1fee3afe1187e83d702
Martin Schmidt <martin.schmidt@epita.fr>
2020-10-18 23:39:54 +0200

    Wordvec impl (#19)
    * charstream: Implement charstream
    
    * main: Dummy usage of charstream
    
    * Implemented a wordvec
    
    * Define constant for easier changes
    
    * Don't lose pointer to previous allocation when realloc fails
    
    * wordvec: return int instead of ptr
    
    Didn't need to return a pointer, therefore int is simpler.
    
    * wordvec: clang-format
    
    * charstream: clang-format
    
    Co-authored-by: Esteban Blanc <estblcsk@gmail.com>

---
+++ include/wordvec.h
@@ -0,0 +1,29 @@
+#ifndef WORDVEC_H
+#define WORDVEC_H
+
+#include <stdlib.h>
+
+#define WORDVEC_DEFAULT_CAP 8
+
+struct wordvec {
+	size_t cap;
+	size_t len;
+	char *vec;
+};
+
+struct wordvec *wordvec_new(void);
+void wordvec_del(struct wordvec *wv);
+
+int wordvec_append(struct wordvec *wv, char c);
+
+static inline const char *wordvec_chars(const struct wordvec *wv)
+{
+	return wv->vec;
+}
+
+static inline size_t wordvec_len(const struct wordvec *wv)
+{
+	return wv->len;
+}
+
+#endif /* ! WORDVEC_H */
---
+++ src/wordvec.c
@@ -0,0 +1,53 @@
+#define _GNU_SOURCE
+#include "wordvec.h"
+
+struct wordvec *wordvec_new(void)
+{
+	struct wordvec *wv = malloc(sizeof(*wv));
+	if (!wv)
+		return NULL;
+
+	wv->len = 0;
+	wv->cap = WORDVEC_DEFAULT_CAP;
+
+	wv->vec = calloc(wv->cap, sizeof(*wv->vec));
+	if (!wv->vec) {
+		free(wv);
+		return NULL;
+	}
+
+	return wv;
+}
+
+void wordvec_del(struct wordvec *wv)
+{
+	free(wv->vec);
+	free(wv);
+}
+
+static int double_capacity(struct wordvec *wv)
+{
+	char *new_vec;
+
+	wv->cap *= 2;
+
+	new_vec = reallocarray(wv->vec, wv->cap, sizeof(*wv->vec));
+	if (!new_vec)
+		return -1;
+
+	wv->vec = new_vec;
+
+	return 0;
+}
+
+int wordvec_append(struct wordvec *wv, char c)
+{
+	if (wv->len >= wv->cap - 1)
+		if (double_capacity(wv) < 0)
+			return -1;
+
+	wv->vec[wv->len] = c;
+	wv->len += 1;
+
+	return 0;
+}
