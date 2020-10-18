3a5795237b14f075b6e622b21b857cf1d71f1f06
Esteban Blanc <estblcsk@gmail.com>
2020-10-18 18:18:46 +0200

    charstream: Implement charstream

--- include/charstream.h
+++ include/charstream.h
@@ -8,8 +8,12 @@
  * Transform input from a file descriptor into a stream of characters.
  */
 
+#define CHARSTREAM_EOF EOF
+#define CHARSTREAM_ERR (-2)
+
 struct charstream {
 	/* Empty for now, it will be filled as implementation is written. */
+	FILE *file;
 };
 
 int charstream_init(struct charstream *cs, FILE *file);
--- src/charstream.c
+++ src/charstream.c
@@ -1,17 +1,36 @@
-#include "charstream.h"
+#include <assert.h>
 
+#include "charstream.h"
 #include "utils.h"
 
 int charstream_init(struct charstream *cs, FILE *file)
 {
-	UNIMPLEMENTED();
+	assert(file);
+
+	cs->file = file;
+
+	return 0;
 }
 char charstream_read(struct charstream *cs)
 {
-	UNIMPLEMENTED();
+	char chr;
+	if (fread(&chr, sizeof(char), 1, cs->file) == 0) {
+		if (feof(cs->file)) {
+			chr = CHARSTREAM_EOF;
+		} else {
+			chr = CHARSTREAM_ERR;
+		}
+		clearerr(cs->file);
+	}
+
+	return chr;
 }
 
 char charstream_peek(struct charstream *cs)
 {
-	UNIMPLEMENTED();
+	char chr = charstream_read(cs);
+	if (chr != CHARSTREAM_ERR)
+		fseek(cs->file, -1, SEEK_CUR);
+
+	return chr;
 }
