d094657c912e4e497acc7e33559e3183e77dffba
Martin Schmidt <martin.schmidt@epita.fr>
2020-10-18 22:10:49 +0200

    Implemented a wordvec

---
+++ include/wordvec.h
@@ -0,0 +1,27 @@
+#ifndef WORDVEC_H
+#define WORDVEC_H
+
+#include <stdlib.h>
+
+struct wordvec {
+  size_t cap;
+  size_t len;
+  char *vec;
+};
+
+struct wordvec *wordvec_new(void);
+void wordvec_del(struct wordvec *wv);
+
+int wordvec_append(struct wordvec *wv, char c);
+
+static inline const char *wordvec_chars(const struct wordvec *wv)
+{
+  return wv->vec;
+}
+
+static inline size_t wordvec_len(const struct wordvec *wv)
+{
+  return wv->len;
+}
+
+#endif /* ! WORDVEC_H */
