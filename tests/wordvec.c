638e70e32b71c43ee62340a5c7de16f5c6b2457b
CohenArthur <arthur.cohen@epita.fr>
2020-10-21 10:38:49 +0200

    tests/wordvec: Add setup and teardown functions

--- tests/wordvec.c
+++ tests/wordvec.c
@@ -1,48 +1,48 @@
 #include <criterion/criterion.h>
 #include "wordvec.h"
 
-Test(wordvec, valid_empty)
+static struct wordvec *vec;
+
+void setup(void)
 {
-    struct wordvec *vec = wordvec_new();
-
-    cr_assert_eq(vec->len, 0);
-    cr_assert_eq(vec->cap, WORDVEC_DEFAULT_CAP);
-    cr_assert_str_eq(vec->vec, "");
-
-    wordvec_del(vec);
+	vec = wordvec_new();
 }
 
-Test(wordvec, valid_small)
+void teardown(void)
 {
-    struct wordvec *vec = wordvec_new();
-
-    char *buf = "HEY";
-    size_t len = strlen(buf);
-
-    for (size_t i = 0; i < len; i++)
-        wordvec_append(vec, buf[i]);
-
-    cr_assert_eq(vec->len, len);
-    cr_assert_eq(vec->cap, WORDVEC_DEFAULT_CAP);
-    cr_assert_eq(vec->vec[vec->len], '\0');
-    cr_assert_str_eq(vec->vec, "HEY");
-
-    wordvec_del(vec);
+	wordvec_del(vec);
 }
 
-Test(wordvec, valid_realloc)
+Test(wordvec, valid_empty, .init = setup, .fini = teardown)
 {
-    struct wordvec *vec = wordvec_new();
+	cr_assert_eq(vec->len, 0);
+	cr_assert_eq(vec->cap, WORDVEC_DEFAULT_CAP);
+	cr_assert_str_eq(vec->vec, "");
+}
 
-    char *buf = "HEY";
-    size_t len = strlen(buf);
+Test(wordvec, valid_small, .init = setup, .fini = teardown)
+{
+	char *buf = "HEY";
+	size_t len = strlen(buf);
 
-    for (size_t i = 0; i < WORDVEC_DEFAULT_CAP * 8; i++)
-        wordvec_append(vec, buf[i % len]);
+	for (size_t i = 0; i < len; i++)
+		wordvec_append(vec, buf[i]);
 
-    cr_assert_eq(vec->len, WORDVEC_DEFAULT_CAP * 8);
-    cr_assert_neq(vec->cap, WORDVEC_DEFAULT_CAP);
-    cr_assert_eq(vec->vec[vec->len], '\0');
+	cr_assert_eq(vec->len, len);
+	cr_assert_eq(vec->cap, WORDVEC_DEFAULT_CAP);
+	cr_assert_eq(vec->vec[vec->len], '\0');
+	cr_assert_str_eq(vec->vec, "HEY");
+}
 
-    wordvec_del(vec);
+Test(wordvec, valid_realloc, .init = setup, .fini = teardown)
+{
+	char *buf = "HEY";
+	size_t len = strlen(buf);
+
+	for (size_t i = 0; i < WORDVEC_DEFAULT_CAP * 8; i++)
+		wordvec_append(vec, buf[i % len]);
+
+	cr_assert_eq(vec->len, WORDVEC_DEFAULT_CAP * 8);
+	cr_assert_neq(vec->cap, WORDVEC_DEFAULT_CAP);
+	cr_assert_eq(vec->vec[vec->len], '\0');
 }
